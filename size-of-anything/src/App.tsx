0e43593ba6dbcea07098b4f6198deab88b5d3ba1
Jovialist <jac@thinkman.com>
2025-06-26 15:10:47 -0700

    Added some cards

---
+++ size-of-anything/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <Card id={35597918} /> {/* Pine Lake */}
+      <Card id={164135822} /> {/* Soaring Eagle Park */}
+      <Card id={1272951719} /> {/* Trossachs */}
+      <Card id={222426053} /> {/* Gilman Village */}
+      <Card id={5586855} /> {/* Disneyland */}
+      <Card id={165475} /> {/* Califnia */}
+    </>
+  );
+}
+
+export default App;
