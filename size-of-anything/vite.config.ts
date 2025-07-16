9f23cac58f4f24198d27f5f9d768960d5a5f05e8
Jovialist <jac@thinkman.com>
2025-07-16 16:38:16 -0700

    Add favicon, change title

--- size-of-anything/vite.config.ts
+++ size-of-anything/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: resolve(__dirname, 'public'),
+  base: '/',
+  server: {
+    fs: {
+      // Allow serving files from one level up the project root
+      allow: ['..']
+    }
+  }
 })
