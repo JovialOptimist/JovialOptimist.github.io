9f85721c5a1cb1aef12d6fffa8317d11a5f8174a
Jovialist <jac@thinkman.com>
2025-07-25 16:15:31 -0700

    Leaflet map implemented
    There's a map, some buttons that don't do anything, and a panel that doesn't do anything, but I mean, hey, at least it looks cool.

--- size-of-anything-2/src/components/map/MapView.tsx
+++ size-of-anything-2/src/components/map/MapView.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+// src/components/map/MapView.tsx
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-/**
- * Leaflet map component
- */
-const MapView: React.FC = () => {
+export default function MapView() {
+  const mapRef = useRef<HTMLDivElement>(null); // Ref to the map container
+  const mapInstanceRef = useRef<L.Map | null>(null); // Store Leaflet map instance
+
+  useEffect(() => {
+    // Prevent reinitialization
+    if (mapRef.current && !mapInstanceRef.current) {
+      const map = L.map(mapRef.current).setView([47.615, -122.035], 13);
+
+      // Add a basic tile layer
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/">OSM</a> contributors',
+      }).addTo(map);
+
+      // Store map instance so we can clean up later
+      mapInstanceRef.current = map;
+    }
+
+    return () => {
+      if (mapInstanceRef.current) {
+        mapInstanceRef.current.remove(); // Clean up Leaflet map
+        mapInstanceRef.current = null;
+      }
+    };
+  }, []);
+
   return (
     <div className="map-container">
-      {/* Leaflet map will be initialized here */}
-      <div id="map"></div>
+      <div id="map" ref={mapRef}></div>
     </div>
   );
-};
-
-export default MapView;
+}
